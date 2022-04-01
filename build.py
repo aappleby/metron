3528876d7749c4938850acf09b2c0cedd6bd5499
Austin Appleby <aappleby@gmail.com>
2022-03-31 18:38:07 -0700

    clean up build script a bit

--- build.py
+++ build.py
@@ -8,10 +8,22 @@ from os import path
 
 # ------------------------------------------------------------------------------
 
+def main():
+  print("Regenerating build.ninja...")
+  build_verilator();
+  build_treesitter();
+  build_metron();
+  build_uart()
+  build_rvsimple()
+  build_rvtiny()
+  build_rvtiny_sync()
+  divider("Done!")
+
+# ------------------------------------------------------------------------------
+
 obj_dir = "obj"
 outfile = open("build.ninja", "w+")
 ninja = ninja_syntax.Writer(outfile)
-
 
 def swap_ext(name, new_ext):
     return path.splitext(name)[0] + new_ext
@@ -42,14 +54,19 @@ ninja.build("build.ninja", "autoupdate", "build.py")
 
 divider("Rules")
 
-ninja.rule("compile_cpp", command="g++ -g ${opt} -std=gnu++2a ${includes} -MMD -MF ${out}.d -c ${in} -o ${out}", deps="gcc", depfile="${out}.d")
-ninja.rule("compile_c",   command="gcc -g ${opt} ${includes} -MMD -MF ${out}.d -c ${in} -o ${out}", deps="gcc", depfile="${out}.d")
-ninja.rule("metron",      command="bin/metron -q -R${src_dir} -O${dst_dir} ${file_list}")
-ninja.rule("verilator",   command="verilator ${includes} --cc ${src_top} -Mdir ${dst_dir}")
-ninja.rule("make",        command="make -C ${dst_dir} -f ${makefile}")
-ninja.rule("link",        command="g++ -g $opt ${in} ${link_libs} -o ${out}")
-ninja.rule("run_test",    command="${in} | grep \"All tests pass.\" && touch ${out}")
-ninja.rule("console",     command="${in}", pool="console")
+ninja.rule("compile_cpp",   command="g++ -g ${opt} -std=gnu++2a ${includes} -MMD -MF ${out}.d -c ${in} -o ${out}", deps="gcc", depfile="${out}.d")
+ninja.rule("compile_c",     command="gcc -g ${opt} ${includes} -MMD -MF ${out}.d -c ${in} -o ${out}", deps="gcc", depfile="${out}.d")
+ninja.rule("metron",        command="bin/metron -q -R${src_dir} -O${dst_dir} ${file_list}")
+ninja.rule("verilator",     command="verilator ${includes} --cc ${src_top} -Mdir ${dst_dir}")
+ninja.rule("make",          command="make -C ${dst_dir} -f ${makefile}")
+ninja.rule("link",          command="g++ -g $opt ${in} ${link_libs} -o ${out}")
+ninja.rule("run_test",      command="${in} | grep \"All tests pass.\" && touch ${out}")
+ninja.rule("console",       command="${in}", pool="console")
+ninja.rule("iverilog",      command="iverilog -g2012 ${defines} ${includes} ${in} -o ${out}")
+ninja.rule("yosys",         command="yosys -p 'read_verilog ${includes} -sv ${in}; dump; synth_ice40 -json ${out};'")
+ninja.rule("nextpnr-ice40", command="nextpnr-ice40 -q --${chip} --package ${package} --json ${in} --asc ${out} --pcf ${pcf}")
+ninja.rule("icepack",       command="icepack ${in} ${out}")
+
 
 # command = "g++ -g $opt -Wl,--whole-archive ${in} -Wl,--no-whole-archive ${link_libs} -o ${out}"
 
@@ -59,6 +76,7 @@ def metronize_dir(src_dir, dst_dir):
     Returns an array of converted filenames.
     """
     divider(f"Metronize {src_dir} -> {dst_dir}")
+
     src_paths = glob.glob(path.join(src_dir, "*.h"))
     src_files = [path.basename(n) for n in src_paths]
     dst_paths = [path.join(dst_dir, swap_ext(n, ".sv")) for n in src_files]
@@ -74,20 +92,21 @@ def metronize_dir(src_dir, dst_dir):
     return dst_paths
 
 
-def verilate_dir(src_dir, src_top, dst_dir):
+def verilate_dir(src_dir, src_files, src_top, dst_dir):
     """
     Run Verilator on all .sv files in the source directory, using "src_top" as
     the top module. Returns the full paths of "V<src_top>.h" and
     "V<src_top>__ALL.obj" in the destination directory.
     """
     divider(f"Verilate {src_top}@{src_dir} -> {dst_dir}")
+
     verilated_make = path.join(dst_dir, f"V{src_top}.mk")
     verilated_hdr = path.join(dst_dir, f"V{src_top}.h")
     verilated_obj = path.join(dst_dir, f"V{src_top}__ALL.o")
 
     # Verilate and generate makefile + header
     ninja.build(rule="verilator",
-                inputs=glob.glob(f"{src_dir}/*.sv"),
+                inputs=src_files,
                 outputs=[verilated_make, verilated_hdr],
                 includes=["-Isrc", f"-I{src_dir}"],
                 src_top=src_top,
@@ -111,6 +130,11 @@ def cpp_binary(bin_name, src_files, src_objs=None, deps=None, **kwargs):
     if deps is None: deps = []
 
     divider(f"Compile {bin_name}")
+
+    # Tack -I onto the includes
+    if kwargs["includes"] is not None:
+      kwargs["includes"] = ["-I" + path for path in kwargs["includes"]] 
+
     for n in src_files:
         obj_name = path.join(obj_dir, swap_ext(n, ".o"))
         ninja.build(outputs=obj_name,
@@ -121,254 +145,273 @@ def cpp_binary(bin_name, src_files, src_objs=None, deps=None, **kwargs):
         src_objs.append(obj_name)
     ninja.build(bin_name, "link", src_objs)
 
+def iverilog_binary(bin_name, src_top, src_files, includes):
+  ninja.build(rule="iverilog",
+              inputs=src_top,
+              implicit=src_files,
+              outputs=bin_name,
+              includes=includes,
+              defines="-DIVERILOG")
+
 # ------------------------------------------------------------------------------
-divider("Verilator libraries")
 
-ninja.build(rule="compile_cpp",
-            inputs="/usr/share/verilator/include/verilated.cpp",
-            outputs="obj/verilated.o")
+def build_verilator():
+  divider("Verilator libraries")
+
+  ninja.build(rule="compile_cpp",
+              inputs="/usr/share/verilator/include/verilated.cpp",
+              outputs="obj/verilated.o")
 
 # ------------------------------------------------------------------------------
-divider("TreeSitter libraries")
 
-treesitter_srcs = [
-    "tree-sitter/lib/src/lib.c",
-    "tree-sitter-cpp/src/parser.c",
-    "tree-sitter-cpp/src/scanner.cc",
-]
 treesitter_objs = []
 
-for n in treesitter_srcs:
-    o = path.join(obj_dir, swap_ext(n, ".o"))
-    ninja.build(
-        o,
-        "compile_c",
-        n,
-        includes="-Itree-sitter/lib/include"
-    )
-    treesitter_objs.append(o)
+def build_treesitter():
+  divider("TreeSitter libraries")
+
+  treesitter_srcs = [
+      "tree-sitter/lib/src/lib.c",
+      "tree-sitter-cpp/src/parser.c",
+      "tree-sitter-cpp/src/scanner.cc",
+  ]
+
+  for n in treesitter_srcs:
+      o = path.join(obj_dir, swap_ext(n, ".o"))
+      ninja.build(
+          o,
+          "compile_c",
+          n,
+          includes="-Itree-sitter/lib/include"
+      )
+      treesitter_objs.append(o)
 
 # ------------------------------------------------------------------------------
 
-cpp_binary(
-    bin_name="bin/metron",
-    src_files=[
-        "src/MetronApp.cpp",
-        "src/MtCursor.cpp",
-        "src/MtMethod.cpp",
-        "src/MtModLibrary.cpp",
-        "src/MtModule.cpp",
-        "src/MtNode.cpp",
-        "src/MtSourceFile.cpp",
-        "src/Platform.cpp",
-    ],
-    includes=["-Itree-sitter/lib/include"],
-    src_objs=treesitter_objs,
-)
+def build_metron():
+  cpp_binary(
+      bin_name="bin/metron",
+      src_files=[
+          "src/MetronApp.cpp",
+          "src/MtCursor.cpp",
+          "src/MtMethod.cpp",
+          "src/MtModLibrary.cpp",
+          "src/MtModule.cpp",
+          "src/MtNode.cpp",
+          "src/MtSourceFile.cpp",
+          "src/Platform.cpp",
+      ],
+      includes=["tree-sitter/lib/include"],
+      src_objs=treesitter_objs,
+  )
 
 # ------------------------------------------------------------------------------
 # Simple Uart testbench
 
-cpp_binary(
-    bin_name="bin/examples/uart",
-    src_files=["examples/uart/main.cpp"],
-    includes=["-Isrc"],
-    # FIXME Why the F does the build break if I don't pass an empty array here?
-    src_objs=[],
-)
+# FIXME move rules out of this block
 
-uart_srcs = metronize_dir("examples/uart/metron", "examples/uart/metron_sv")
+def build_uart():
+  cpp_binary(
+      bin_name="bin/examples/uart",
+      src_files=["examples/uart/main.cpp"],
+      includes=["src"],
+      # FIXME Why the F does the build break if I don't pass an empty array here?
+      src_objs=[],
+  )
 
-uart_vhdr, uart_vobj = verilate_dir(
-    src_dir="examples/uart/metron_sv",
-    src_top="uart_top",
-    dst_dir="examples/uart/metron_vl",
-)
+  uart_srcs = metronize_dir("examples/uart/metron", "examples/uart/metron_sv")
 
-cpp_binary(
-    bin_name="bin/examples/uart_vl",
-    src_files=["examples/uart/main_vl.cpp"],
-    includes=[
-        "-Isrc",
-        "-Itests",
-        "-Iobj/examples/uart",
-        "-I/usr/local/share/verilator/include"
-    ],
-    src_objs=["obj/verilated.o", uart_vobj],
-    deps=[uart_vhdr]
-)
+  uart_vhdr, uart_vobj = verilate_dir(
+      src_dir="examples/uart/metron_sv",
+      src_files = uart_srcs,
+      src_top="uart_top",
+      dst_dir="examples/uart/metron_vl",
+  )
 
-divider("Icarus Verilog uart testbench")
+  cpp_binary(
+      bin_name="bin/examples/uart_vl",
+      src_files=["examples/uart/main_vl.cpp"],
+      includes=[
+          "src",
+          "tests",
+          "obj/examples/uart",
+          "/usr/local/share/verilator/include"
+      ],
+      src_objs=["obj/verilated.o", uart_vobj],
+      deps=[uart_vhdr]
+  )
 
-uart_includes = [
-    "-Isrc",
-    "-Iexamples/uart",
-    "-Iexamples/uart/metron_sv"
-]
+  divider("Icarus Verilog uart testbench")
 
-ninja.rule(name="iverilog",
-           command="iverilog -g2012 ${defines} ${includes} ${in} -o ${out}")
+  uart_includes = [
+      "-Isrc",
+      "-Iexamples/uart",
+      "-Iexamples/uart/metron_sv"
+  ]
 
-ninja.build(rule="iverilog",
-            inputs="examples/uart/uart_test_iv.sv",
-            implicit=uart_srcs,
-            outputs="bin/examples/uart_iv",
-            includes=uart_includes,
-            defines="-DIVERILOG")
+  iverilog_binary(
+    bin_name="bin/examples/uart_iv",
+    src_top="examples/uart/uart_test_iv.sv",
+    src_files=uart_srcs,
+    includes=uart_includes);
 
-divider("Yosys/NextPNR uart testbench")
+  divider("Yosys/NextPNR uart testbench")
 
-ninja.rule(name="yosys",
-           command="yosys -p 'read_verilog ${includes} -sv ${in}; dump; synth_ice40 -json ${out};'")
+  ninja.build(rule="yosys",
+              inputs="examples/uart/uart_test_ice40.sv",
+              implicit=uart_srcs,
+              outputs="obj/examples/uart/uart_test_ice40.json",
+              includes=uart_includes)
 
-ninja.build(rule="yosys",
-            inputs="examples/uart/uart_test_ice40.sv",
-            implicit=uart_srcs,
-            outputs="obj/examples/uart/uart_test_ice40.json",
-            includes=uart_includes)
+  ninja.build(rule="nextpnr-ice40",
+              inputs="obj/examples/uart/uart_test_ice40.json",
+              outputs="obj/examples/uart/uart_test_ice40.asc",
+              chip="hx8k",
+              package="ct256",
+              pcf="examples/uart/ice40-hx8k-b-evn.pcf")
 
-ninja.rule(name="nextpnr-ice40",
-           command="nextpnr-ice40 -q --${chip} --package ${package} --json ${in} --asc ${out} --pcf ${pcf}")
-
-ninja.build(rule="nextpnr-ice40",
-            inputs="obj/examples/uart/uart_test_ice40.json",
-            outputs="obj/examples/uart/uart_test_ice40.asc",
-            chip="hx8k",
-            package="ct256",
-            pcf="examples/uart/ice40-hx8k-b-evn.pcf")
-
-
-ninja.rule(name="icepack", command="icepack ${in} ${out}")
-ninja.build(rule="icepack",
-            inputs="obj/examples/uart/uart_test_ice40.asc",
-            outputs="obj/examples/uart/uart_test_ice40.bin")
+  ninja.build(rule="icepack",
+              inputs="obj/examples/uart/uart_test_ice40.asc",
+              outputs="obj/examples/uart/uart_test_ice40.bin")
 
 # build out/uart_test_ice40.bin   : iceprog out/uart_test_ice40.asc
 
 # ------------------------------------------------------------------------------
 # RVSimple
 
-cpp_binary(
-    bin_name="bin/examples/rvsimple",
-    src_files=["examples/rvsimple/main.cpp"],
-    includes=["-Isrc"],
-    opt="-O3",
-)
+def build_rvsimple():
+  cpp_binary(
+      bin_name="bin/examples/rvsimple",
+      src_files=["examples/rvsimple/main.cpp"],
+      includes=["src"],
+      opt="-O3",
+  )
 
-rvsimple_metron_srcs = metronize_dir(
-    "examples/rvsimple/metron", "examples/rvsimple/metron_sv")
+  rvsimple_metron_srcs = metronize_dir(
+      "examples/rvsimple/metron", "examples/rvsimple/metron_sv")
 
-rvsimple_metron_vhdr, rvsimple_metron_vobj = verilate_dir(
-    src_dir="examples/rvsimple/metron_sv",
-    src_top="toplevel",
-    dst_dir="examples/rvsimple/metron_vl"
-)
+  rvsimple_metron_vhdr, rvsimple_metron_vobj = verilate_dir(
+      src_dir="examples/rvsimple/metron_sv",
+      src_files=rvsimple_metron_srcs,
+      src_top="toplevel",
+      dst_dir="examples/rvsimple/metron_vl"
+  )
 
-cpp_binary(
-    bin_name="bin/examples/rvsimple_vl",
-    src_files=["examples/rvsimple/main_vl.cpp"],
-    includes=[
-        "-Isrc",
-        "-Itests",
-        "-Iobj/examples/rvsimple",
-        "-I/usr/local/share/verilator/include"
-    ],
-    src_objs=["obj/verilated.o", rvsimple_metron_vobj],
-    deps=[rvsimple_metron_vhdr]
-)
+  cpp_binary(
+      bin_name="bin/examples/rvsimple_vl",
+      src_files=["examples/rvsimple/main_vl.cpp"],
+      includes=[
+          "src",
+          "tests",
+          "obj/examples/rvsimple",
+          "/usr/local/share/verilator/include"
+      ],
+      src_objs=["obj/verilated.o", rvsimple_metron_vobj],
+      deps=[rvsimple_metron_vhdr]
+  )
 
-rvsimple_reference_vhdr, rvsimple_reference_vobj = verilate_dir(
-    src_dir="examples/rvsimple/reference",
-    src_top="toplevel",
-    dst_dir="examples/rvsimple/reference_vl"
-)
+  rvsimple_reference_vhdr, rvsimple_reference_vobj = verilate_dir(
+      src_dir="examples/rvsimple/reference",
+      src_files=glob.glob("examples/rvsimple/reference/*.sv"),
+      src_top="toplevel",
+      dst_dir="examples/rvsimple/reference_vl"
+  )
 
-cpp_binary(
-    bin_name="bin/examples/rvsimple_ref",
-    src_files=["examples/rvsimple/main_ref_vl.cpp"],
-    includes=[
-        "-Isrc",
-        "-Itests",
-        "-Iobj/examples/rvsimple",
-        "-I/usr/local/share/verilator/include"
-    ],
-    src_objs=["obj/verilated.o", rvsimple_reference_vobj],
-    deps=[rvsimple_reference_vhdr]
-)
+  cpp_binary(
+      bin_name="bin/examples/rvsimple_ref",
+      src_files=["examples/rvsimple/main_ref_vl.cpp"],
+      includes=[
+          "src",
+          "tests",
+          "obj/examples/rvsimple",
+          "/usr/local/share/verilator/include"
+      ],
+      src_objs=["obj/verilated.o", rvsimple_reference_vobj],
+      deps=[rvsimple_reference_vhdr]
+  )
+
 
 # ------------------------------------------------------------------------------
 # RVTiny
 
-cpp_binary(
-    bin_name="bin/examples/rvtiny",
-    src_files=["examples/rvtiny/main.cpp"],
-    includes=[
-        "-I.",
-        "-Isrc"
-    ],
-    opt="-O3",
-)
+def build_rvtiny():
+  mt_root="examples/rvtiny/metron"
+  sv_root="examples/rvtiny/metron_sv"
+  vl_root="examples/rvtiny/metron_vl"
 
-rvtiny_metron_srcs = metronize_dir(
-    "examples/rvtiny/metron", "examples/rvtiny/metron_sv")
+  obj_root="obj/exampples/rvtiny"
+  bin_root="bin/examples"
 
-rvtiny_metron_vhdr, rvtiny_metron_vobj = verilate_dir(
-    src_dir="examples/rvtiny/metron_sv",
-    src_top="toplevel",
-    dst_dir="examples/rvtiny/metron_vl"
-)
+  cpp_binary(
+      bin_name="bin/examples/rvtiny",
+      src_files=["examples/rvtiny/main.cpp"],
+      includes=[
+          ".",
+          "src"
+      ],
+      opt="-O3",
+  )
 
-cpp_binary(
-    bin_name="bin/examples/rvtiny_vl",
-    src_files=["examples/rvtiny/main_vl.cpp"],
-    includes=[
-        "-I.",
-        "-Isrc",
-        "-Iobj/examples/rvtiny",
-        "-I/usr/local/share/verilator/include"
-    ],
-    src_objs=["obj/verilated.o", rvtiny_metron_vobj],
-    deps=[rvtiny_metron_vhdr]
-)
+  rvtiny_metron_srcs = metronize_dir(mt_root, sv_root)
+
+  rvtiny_metron_vhdr, rvtiny_metron_vobj = verilate_dir(
+      src_dir=sv_root,
+      src_files=rvtiny_metron_srcs,
+      src_top="toplevel",
+      dst_dir=vl_root
+  )
+
+  cpp_binary(
+      bin_name="bin/examples/rvtiny_vl",
+      src_files=["examples/rvtiny/main_vl.cpp"],
+      includes=[
+          ".",
+          "src",
+          "obj/examples/rvtiny",
+          "/usr/local/share/verilator/include"
+      ],
+      src_objs=["obj/verilated.o", rvtiny_metron_vobj],
+      deps=[rvtiny_metron_vhdr]
+  )
+
 
 # ------------------------------------------------------------------------------
 # RVTiny_Sync - synchronous-mem-only version of RVTiny
 
-cpp_binary(
-    bin_name="bin/examples/rvtiny_sync",
-    src_files=["examples/rvtiny_sync/main.cpp"],
-    includes=[
-        "-I.",
-        "-Isrc"
-    ],
-    opt="-O3",
-)
+def build_rvtiny_sync():
+  cpp_binary(
+      bin_name="bin/examples/rvtiny_sync",
+      src_files=["examples/rvtiny_sync/main.cpp"],
+      includes=[
+          ".",
+          "src"
+      ],
+      opt="-O3",
+  )
 
-rvtiny_sync_metron_sv = metronize_dir(
-    "examples/rvtiny_sync/metron", "examples/rvtiny_sync/metron_sv")
+  rvtiny_sync_metron_sv = metronize_dir(
+      "examples/rvtiny_sync/metron", "examples/rvtiny_sync/metron_sv")
 
-rvtiny_sync_metron_vl_h, rvtiny_sync_metron_vl_o = verilate_dir(
-    src_dir="examples/rvtiny_sync/metron_sv",
-    src_top="toplevel",
-    dst_dir="examples/rvtiny_sync/metron_vl"
-)
+  rvtiny_sync_metron_vl_h, rvtiny_sync_metron_vl_o = verilate_dir(
+      src_dir="examples/rvtiny_sync/metron_sv",
+      src_files=rvtiny_sync_metron_sv,
+      src_top="toplevel",
+      dst_dir="examples/rvtiny_sync/metron_vl"
+  )
 
-cpp_binary(
-    bin_name="bin/examples/rvtiny_sync_vl",
-    src_files=["examples/rvtiny_sync/main_vl.cpp"],
-    includes=[
-        "-I.",
-        "-Isrc",
-        "-Iobj/examples/rvtiny_sync",
-        "-I/usr/local/share/verilator/include"
-    ],
-    src_objs=["obj/verilated.o", rvtiny_sync_metron_vl_o],
-    deps=[rvtiny_sync_metron_vl_h]
-)
+  cpp_binary(
+      bin_name="bin/examples/rvtiny_sync_vl",
+      src_files=["examples/rvtiny_sync/main_vl.cpp"],
+      includes=[
+          ".",
+          "src",
+          "obj/examples/rvtiny_sync",
+          "/usr/local/share/verilator/include"
+      ],
+      src_objs=["obj/verilated.o", rvtiny_sync_metron_vl_o],
+      deps=[rvtiny_sync_metron_vl_h]
+  )
 
 # ------------------------------------------------------------------------------
-
-divider("Done!")
 
 
 """"
@@ -383,3 +426,6 @@ divider("Done!")
 # default bin/examples/rvsimple/main_generated
 # default bin/examples/rvsimple/main_rtl
 """
+
+if __name__ == "__main__":
+  main()

d34f18427dd03c5d81d44ec616163b0e11f729c9
Austin Appleby <aappleby@gmail.com>
2022-04-03 21:29:40 -0700

    so far so good

---
+++ tests/metron_good/basic_output.h
@@ -0,0 +1,17 @@
+#include "metron_tools.h"
+
+// Getter methods should turn into outputs.
+
+class Module {
+public:
+
+  logic<7> get_reg() const {
+    return my_reg;
+  }
+
+  void tick() {
+    my_reg = my_reg + 1;
+  }
+
+  logic<7> my_reg;
+};
---
+++ tests/metron_sv/basic_output.sv
@@ -0,0 +1,24 @@
+`include "metron_tools.sv"
+
+// Getter methods should turn into outputs.
+
+module Module
+(
+  input logic clock,
+  output logic[6:0] my_reg,
+  output logic[6:0] get_reg
+);
+/*public:*/
+
+  always_comb begin
+    get_reg = my_reg;
+  end
+
+  task tick(); 
+    my_reg <= my_reg + 1;
+  endtask
+  always_ff @(posedge clock) tick();
+
+  /*logic<7> my_reg;*/
+endmodule
+

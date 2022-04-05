0185981a2f5933138458976ffdaacb9ca2b11fea
Austin Appleby <aappleby@gmail.com>
2022-04-05 14:13:34 -0700

    fix submod_bindings function names

--- tests/metron_good/submod_bindings.h
+++ tests/metron_good/submod_bindings.h
@@ -8,8 +8,8 @@ public:
 
   logic<8> sum_a(logic<8> a1, logic<8> a2) const { return a1 + a2; }
   logic<8> sum_b(logic<8> b1, logic<8> b2) const { return b1 + b2; }
+  logic<8> sum_c(logic<8> c1, logic<8> c2) const { return c1 + c2; }
   logic<8> sum_d(logic<8> d1, logic<8> d2) const { return d1 + d2; }
-  logic<8> sum_e(logic<8> e1, logic<8> e2) const { return e1 + e2; }
 };
 
 class Module {
@@ -26,15 +26,15 @@ public:
         result = submod.sum_b(3,4);
       }
       else {
-        // Only sum_c's bindings should be here.
+        // Only sum_b's bindings should be here.
         result = submod.sum_b(5,6);
       }
 
-      // All of d/e/f's bindings should be here.
-      if (submod.sum_d(7, 8))
-        result = submod.sum_e(9,10);
+      // All of c/d's bindings should be here.
+      if (submod.sum_c(7, 8))
+        result = submod.sum_d(9,10);
       else
-        result = submod.sum_e(11,12);
+        result = submod.sum_d(11,12);
     }
 
     return result;
--- tests/metron_sv/submod_bindings.sv
+++ tests/metron_sv/submod_bindings.sv
@@ -10,21 +10,21 @@ module Submod
   input logic[7:0] a2,
   input logic[7:0] b1,
   input logic[7:0] b2,
+  input logic[7:0] c1,
+  input logic[7:0] c2,
   input logic[7:0] d1,
   input logic[7:0] d2,
-  input logic[7:0] e1,
-  input logic[7:0] e2,
   output logic[7:0] sum_a,
   output logic[7:0] sum_b,
-  output logic[7:0] sum_d,
-  output logic[7:0] sum_e
+  output logic[7:0] sum_c,
+  output logic[7:0] sum_d
 );
 /*public:*/
 
   always_comb begin sum_a = a1 + a2; end
   always_comb begin sum_b = b1 + b2; end
+  always_comb begin sum_c = c1 + c2; end
   always_comb begin sum_d = d1 + d2; end
-  always_comb begin sum_e = e1 + e2; end
 endmodule
 
 module Module
@@ -50,23 +50,23 @@ module Module
         result = submod_sum_b;
       end
       else begin
-        // Only sum_c's bindings should be here.
+        // Only sum_b's bindings should be here.
         submod_b1 = 5;
         submod_b2 = 6;
         result = submod_sum_b;
       end
 
-      // All of d/e/f's bindings should be here.
-      submod_d1 = 7;
-      submod_d2 = 8;
-      submod_e1 = 9;
-      submod_e2 = 10;
-      submod_e1 = 11;
-      submod_e2 = 12;
-      if (submod_sum_d)
-        result = submod_sum_e;
+      // All of c/d's bindings should be here.
+      submod_c1 = 7;
+      submod_c2 = 8;
+      submod_d1 = 9;
+      submod_d2 = 10;
+      submod_d1 = 11;
+      submod_d2 = 12;
+      if (submod_sum_c)
+        result = submod_sum_d;
       else
-        result = submod_sum_e;
+        result = submod_sum_d;
     end
 
     test_bindings = result;
@@ -79,28 +79,28 @@ module Module
     .a2(submod_a2), 
     .b1(submod_b1), 
     .b2(submod_b2), 
+    .c1(submod_c1), 
+    .c2(submod_c2), 
     .d1(submod_d1), 
     .d2(submod_d2), 
-    .e1(submod_e1), 
-    .e2(submod_e2), 
     // Outputs
     .sum_a(submod_sum_a), 
     .sum_b(submod_sum_b), 
-    .sum_d(submod_sum_d), 
-    .sum_e(submod_sum_e)
+    .sum_c(submod_sum_c), 
+    .sum_d(submod_sum_d)
   );
   logic[7:0] submod_a1;
   logic[7:0] submod_a2;
   logic[7:0] submod_b1;
   logic[7:0] submod_b2;
+  logic[7:0] submod_c1;
+  logic[7:0] submod_c2;
   logic[7:0] submod_d1;
   logic[7:0] submod_d2;
-  logic[7:0] submod_e1;
-  logic[7:0] submod_e2;
   logic[7:0] submod_sum_a;
   logic[7:0] submod_sum_b;
+  logic[7:0] submod_sum_c;
   logic[7:0] submod_sum_d;
-  logic[7:0] submod_sum_e;
 
 endmodule
 

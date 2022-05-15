bba0aa4931ab3247813e97ed9f17a482f3b9fd3a
Austin Appleby <aappleby@gmail.com>
2022-05-15 01:43:52 -0700

    pretty!

--- docs/app/main.js
+++ docs/app/main.js
@@ -20,16 +20,13 @@ var log_text = document.getElementById("log_text");
 var log_cmdline = document.getElementById("log_cmdline");
 
 //------------------------------------------------------------------------------
-// Set up Emscripten module
+// Set up Emscripten module to run main() when it's ready.
 
 var mod_options = window.Module;
-
 mod_options.noInitialRun = true;
-
 mod_options.print = function(text) {
   term.write(text + "\n");
 }
-
 mod_options.onRuntimeInitialized = function() {
   console.log("mod_options.onRuntimeInitialized " + performance.now());
 }
@@ -44,14 +41,11 @@ const highlight = editor => {
   hljs.highlightElement(editor)
 }
 
-
 const src_jar = CodeJar(src_text, highlight, {tab:"  "});
-
-// readOnly?
 const dst_jar = CodeJar(dst_text, highlight, {tab:"  "});
 
 //------------------------------------------------------------------------------
-// Set up terminal
+// Set up Terminal
 
 var term = new Terminal({
   fontSize: 14,
@@ -98,12 +92,13 @@ function dump_fs(FS, path, src_paths) {
 
 function main(mod) {
   console.log("main() " + performance.now());
+
+  // Load all the filenames in our filesystem.
   let src_paths = [];
   dump_fs(mod.FS, ".", src_paths);
-  //console.log(src_paths);
-
   src_paths.sort();
 
+  // Put all those names into the selector widget.
   src_selector.innerHTML = "";
   for (let i = 0; i < src_paths.length; i++) {
     var opt = document.createElement('option');
@@ -112,23 +107,21 @@ function main(mod) {
     src_selector.appendChild(opt);
   }
 
+  // Load scratch.h by default.
   var initial_file = "./examples/scratch.h";
   src_selector.selectedIndex = src_paths.indexOf(initial_file);
   src_jar.updateCode(new TextDecoder().decode(mod.FS.readFile(initial_file)));
 
+  // When the user changes the selected file, load and convert it (updateCode
+  // doesn't trigger onUpdate)
   src_selector.addEventListener('change', function(event) {
-    // Load the new file
     var selector_name = src_selector.options[src_selector.selectedIndex].value;
     src_jar.updateCode(new TextDecoder().decode(mod.FS.readFile(selector_name)));
-
-    // And convert it
     convert(mod);
   });
 
   // Re-convert whenever the user edits the source pane or changes an option.
-  //src_text.addEventListener("input", () => convert(mod));
   src_jar.onUpdate(() => convert(mod));
-
   opt_help.addEventListener("change", () => convert(mod));
   opt_echo.addEventListener("change", () => convert(mod));
   opt_dump.addEventListener("change", () => convert(mod));
@@ -136,21 +129,15 @@ function main(mod) {
   opt_verbose.addEventListener("change", () => convert(mod));
   opt_save.addEventListener("change", () => convert(mod));
 
+  // Start with save mode on and trigger convert so we generate scratch.sv.
   opt_save.checked = true;
-
   convert(mod);
-  fitAddon.fit();
 }
 
 //------------------------------------------------------------------------------
+// Convert the selected .h file to Verilog and update the panes.
 
 function convert(mod) {
-  term.clear();
-  //term.setOption('theme', { fontSize: 22 })
-  //fitAddon.fit();
-
-  dst_jar.updateCode("");
-
   var selector_name = src_selector.options[src_selector.selectedIndex].value;
 
   // Write the potentially modified source back to the filesystem
@@ -160,49 +147,41 @@ function convert(mod) {
   // Abort conversion if file doesn't end with ".h"
   if (!selector_name.endsWith(".h")) return;
 
-  // Delete any existing output file
+  // Change dst pane title & delete any existing output file
   var file_out = selector_name.substr(0, selector_name.lastIndexOf('.')) + ".sv";
-
   dst_title.innerText = file_out;
-
-  //console.log("unlinking " + file_out);
   try {
-    FS.unlink(file_out);
-    //console.log("unlink ok");
+    mod.FS.unlink(file_out);
   }
   catch {
-    //console.log("unlink fail");
   }
 
   // Run Metron
   var path = selector_name.split("/");
   var root = path.slice(0, path.length - 1).join("/");
   var file = path[path.length - 1];
-  var file_out = selector_name.substr(0, selector_name.lastIndexOf('.')) + ".sv";
 
   var args = [];
-
   if (opt_echo.checked) args.push("-e");
   if (opt_dump.checked) args.push("--dump");
   if (opt_quiet.checked) args.push("-q");
   if (opt_verbose.checked) args.push("-v");
   if (opt_save.checked) args.push("-s");
-
   args.push("-r");
   args.push(root);
   args.push(file);
-
   if (opt_help.checked) args = ["-h"];
 
+  term.clear();
   log_cmdline.innerText = "metron " + args.join(" ");
-
   mod.callMain(args);
 
-  // Read the output file back to the dst pane
+  // Read the output file back to the dst pane, or clear if no file.
   try {
     dst_jar.updateCode(new TextDecoder().decode(mod.FS.readFile(file_out)));
   }
   catch {
+    dst_jar.updateCode("");
   }
 }
 

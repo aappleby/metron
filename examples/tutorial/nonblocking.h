14234a8d24baa1cb2ee37fa5f0afff2f3b199bee
Austin Appleby <aappleby@gmail.com>
2022-05-26 02:46:08 -0700

    enough for tonight

--- examples/tutorial/nonblocking.h
+++ examples/tutorial/nonblocking.h
@@ -1,15 +1,8 @@
 class NonBlockingAssignment {
 public:
   int count;
-  int update_bad() {
+  void update() {
     count = count + 1;
-    return count;
+    int dummy = count;
   }
-  /*
-  int update_good() {
-    int new_count = count + 1;
-    count = new_count;
-    return new_count;
-  }
-  */
 };

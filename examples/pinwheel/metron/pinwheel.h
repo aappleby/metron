f6c2f3a57292e03b8249acc3bedd2826311a2735
Austin Appleby <aappleby@gmail.com>
2022-11-02 00:15:49 -0700

    checkpoint

--- examples/pinwheel/metron/pinwheel.h
+++ examples/pinwheel/metron/pinwheel.h
@@ -3,10 +3,12 @@
 #include "constants.h"
 
 struct registers_p0 {
+  logic<2>  hart;
   logic<32> pc;
 };
 
 struct signals_p20 {
+  logic<2>  hart;
   logic<5>  reg_addr;
   logic<32> reg_data;
   logic<1>  reg_wren;
@@ -14,6 +16,7 @@ struct signals_p20 {
 };
 
 struct registers_p1 {
+  logic<2>  hart;
   logic<32> pc;
   logic<32> imm;
   logic<5>  op;
@@ -23,11 +26,13 @@ struct registers_p1 {
 };
 
 struct signals_p01 {
+  logic<2>  hart;
   logic<5> addr1;
   logic<5> addr2;
 };
 
 struct registers_p2 {
+  logic<2>  hart;
   logic<32> pc;
   logic<5>  op;
   logic<3>  f3;
@@ -37,6 +42,7 @@ struct registers_p2 {
 };
 
 struct signals_p12 {
+  logic<2>  hart;
   logic<32> addr;
   logic<32> data;
   logic<4>  byte_wren;
@@ -91,6 +97,10 @@ class Pinwheel {
     reg_p1 = {0};
     reg_p2 = {0};
 
+    reg_p0.hart = 0;
+    reg_p1.hart = 1;
+    reg_p2.hart = 2;
+
     sig_p20 = {0};
     sig_p01 = {0};
     sig_p12 = {0};
@@ -119,6 +129,7 @@ class Pinwheel {
       case OP_AUIPC:  imm = imm_u; break;
     }
 
+    reg_p1.hart = reg_p0.hart;
     reg_p1.pc = reg_p0.pc;
     reg_p1.op = b5(p0_prog_read, 2);
     reg_p1.imm = imm;
@@ -126,6 +137,7 @@ class Pinwheel {
     reg_p1.f7 = b7(p0_prog_read, 25);
     reg_p1.reg_write_addr = b5(p0_prog_read, 7);
 
+    sig_p01.hart  = reg_p0.hart;
     sig_p01.addr1 = b5(p0_prog_read, 15);
     sig_p01.addr2 = b5(p0_prog_read, 20);
   }
@@ -195,6 +207,7 @@ class Pinwheel {
       case 7: alu_result = alu_in_a & alu_in_b; break;
     }
 
+    reg_p2.hart = reg_p1.hart;
     reg_p2.alu_result = alu_result;
     reg_p2.reg_write_addr = reg_p1.reg_write_addr;
     reg_p2.op = reg_p1.op;
@@ -203,6 +216,7 @@ class Pinwheel {
 
     // Data mem read/write
 
+    sig_p12.hart = reg_p1.hart;
     sig_p12.addr = alu_result;
     sig_p12.data    = p1_regs_read_data2 << (8 * b2(alu_result));
     if (reg_p1.op == OP_STORE) {
@@ -217,23 +231,22 @@ class Pinwheel {
 
   //----------------------------------------
 
-  signals_p20 delta20(logic<32> p2_data_read, logic<32> p2_reg_alu_result) {
+  static void delta20(const registers_p2& reg_p2, const logic<32> p2_data_read, signals_p20& sig_p20, registers_p0& reg_p0) {
     logic<32> unpacked_data;
     switch (reg_p2.f3) {
-      case 0: unpacked_data = sign_extend<32>(b8(p2_data_read, 8 * b2(p2_reg_alu_result))); break;
-      case 1: unpacked_data = sign_extend<32>(b16(p2_data_read, 8 * b2(p2_reg_alu_result))); break;
+      case 0: unpacked_data = sign_extend<32>(b8(p2_data_read, 8 * b2(reg_p2.alu_result))); break;
+      case 1: unpacked_data = sign_extend<32>(b16(p2_data_read, 8 * b2(reg_p2.alu_result))); break;
       case 2: unpacked_data = p2_data_read; break;
       case 3: unpacked_data = p2_data_read; break;
-      case 4: unpacked_data = b8(p2_data_read, 8 * b2(p2_reg_alu_result)); break;
-      case 5: unpacked_data = b16(p2_data_read, 8 * b2(p2_reg_alu_result)); break;
+      case 4: unpacked_data = b8(p2_data_read, 8 * b2(reg_p2.alu_result)); break;
+      case 5: unpacked_data = b16(p2_data_read, 8 * b2(reg_p2.alu_result)); break;
       case 6: unpacked_data = p2_data_read; break;
       case 7: unpacked_data = p2_data_read; break;
     }
 
-    signals_p20 sig_p20;
     switch(reg_p2.op) {
-      case OP_ALU:     sig_p20.reg_wren = true;  sig_p20.reg_data = p2_reg_alu_result;    break;
-      case OP_ALUI:    sig_p20.reg_wren = true;  sig_p20.reg_data = p2_reg_alu_result;    break;
+      case OP_ALU:     sig_p20.reg_wren = true;  sig_p20.reg_data = reg_p2.alu_result;    break;
+      case OP_ALUI:    sig_p20.reg_wren = true;  sig_p20.reg_data = reg_p2.alu_result;    break;
       case OP_LOAD:    sig_p20.reg_wren = true;  sig_p20.reg_data = unpacked_data; break;
       case OP_STORE:   sig_p20.reg_wren = false; sig_p20.reg_data = b32(DONTCARE); break;
       case OP_BRANCH:  sig_p20.reg_wren = false; sig_p20.reg_data = b32(DONTCARE); break;
@@ -243,17 +256,25 @@ class Pinwheel {
       case OP_AUIPC:   sig_p20.reg_wren = true;  sig_p20.reg_data = reg_p2.alu_result;    break;
     }
 
-    reg_p0.pc = reg_p2.pc_sel ? b32(p2_reg_alu_result) : b32(reg_p2.pc + 4);
+    reg_p0.hart = reg_p2.hart;
+    reg_p0.pc = reg_p2.pc_sel ? b32(reg_p2.alu_result) : b32(reg_p2.pc + 4);
 
+    sig_p20.hart     = reg_p2.hart;
     sig_p20.reg_addr = reg_p2.reg_write_addr;
     sig_p20.mem_addr = reg_p0.pc;
-
-    return sig_p20;
   }
 
   //----------------------------------------
 
   void tick(logic<1> reset_in) {
+    registers_p0 old_reg_p0 = reg_p0;
+    registers_p1 old_reg_p1 = reg_p1;
+    registers_p2 old_reg_p2 = reg_p2;
+
+    signals_p20 old_sig_p20 = sig_p20;
+    signals_p01 old_sig_p01 = sig_p01;
+    signals_p12 old_sig_p12 = sig_p12;
+
     logic<2> next_phase = 0;
 
     if (reset_in) {
@@ -262,38 +283,37 @@ class Pinwheel {
       return;
     }
 
-    logic<32> prog_read = prog_mem[b14(sig_p20.mem_addr, 2)];
+    if ((old_sig_p12.hart == 0) && old_sig_p12.byte_wren) {
+      logic<32> old_mem = data_mem[b15(old_sig_p12.addr, 2)];
+      if (old_sig_p12.byte_wren[0]) old_mem = (old_mem & 0xFFFFFF00) | (old_sig_p12.data & 0x000000FF);
+      if (old_sig_p12.byte_wren[1]) old_mem = (old_mem & 0xFFFF00FF) | (old_sig_p12.data & 0x0000FF00);
+      if (old_sig_p12.byte_wren[2]) old_mem = (old_mem & 0xFF00FFFF) | (old_sig_p12.data & 0x00FF0000);
+      if (old_sig_p12.byte_wren[3]) old_mem = (old_mem & 0x00FFFFFF) | (old_sig_p12.data & 0xFF000000);
+      data_mem[b15(old_sig_p12.addr, 2)] = old_mem;
+    }
 
+    if ((old_sig_p20.hart == 0) && old_sig_p20.reg_addr && old_sig_p20.reg_wren) {
+      regs[old_sig_p20.reg_addr] = old_sig_p20.reg_data;
+    }
+
+    logic<32> old_prog_read = prog_mem[b14(old_sig_p20.mem_addr, 2)];
+    logic<32> old_data_read = data_mem[b15(old_sig_p12.addr, 2)];
+    logic<32> old_regs_read_data1 = regs[old_sig_p01.addr1];
+    logic<32> old_regs_read_data2 = regs[old_sig_p01.addr2];
+
+    delta01(old_reg_p0, old_prog_read, sig_p01, reg_p1);
     if (phase == 0) {
-      delta01(reg_p0, prog_read, sig_p01, reg_p1);
       next_phase = 1;
     }
 
-
+    delta12(old_reg_p1, old_regs_read_data1, old_regs_read_data2, sig_p12, reg_p2);
     if (phase == 1) {
-      logic<32> regs_read_data1 = regs[sig_p01.addr1];
-      logic<32> regs_read_data2 = regs[sig_p01.addr2];
-      delta12(reg_p1, regs_read_data1, regs_read_data2, sig_p12, reg_p2);
       next_phase = 2;
     }
 
-    if (sig_p12.byte_wren) {
-      logic<32> old_mem = data_mem[b15(sig_p12.addr, 2)];
-      if (sig_p12.byte_wren[0]) old_mem = (old_mem & 0xFFFFFF00) | (sig_p12.data & 0x000000FF);
-      if (sig_p12.byte_wren[1]) old_mem = (old_mem & 0xFFFF00FF) | (sig_p12.data & 0x0000FF00);
-      if (sig_p12.byte_wren[2]) old_mem = (old_mem & 0xFF00FFFF) | (sig_p12.data & 0x00FF0000);
-      if (sig_p12.byte_wren[3]) old_mem = (old_mem & 0x00FFFFFF) | (sig_p12.data & 0xFF000000);
-      data_mem[b15(sig_p12.addr, 2)] = old_mem;
-    }
-    logic<32> data_read = data_mem[b15(sig_p12.addr, 2)];
-
     if (phase == 2) {
-      sig_p20 = delta20(data_read, reg_p2.alu_result);
+      delta20(old_reg_p2, old_data_read, sig_p20, reg_p0);
       next_phase = 0;
-    }
-
-    if (sig_p20.reg_addr && sig_p20.reg_wren) {
-      regs[sig_p20.reg_addr] = sig_p20.reg_data;
     }
 
     phase = next_phase;

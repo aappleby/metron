915ed6c3511b80f63323fbf6982d047a0ed8aa97
Austin Appleby <aappleby@gmail.com>
2023-08-13 20:04:01 -0700

    checkpoint

--- metron/parser/Cursor.cpp
+++ metron/parser/Cursor.cpp
@@ -1,1 +1,154 @@
 #include "Cursor.hpp"
+
+#include "metrolib/core/Log.h"
+
+Cursor::Cursor(CSourceRepo* repo, CSourceFile* source, std::string* out) {
+  this->repo = repo;
+  this->source = source;
+  this->out = out;
+}
+
+//------------------------------------------------------------------------------
+
+/*
+void Cursor::push_indent(MnNode body) {
+  assert(body.sym == sym_compound_statement ||
+         body.sym == sym_field_declaration_list);
+
+  auto n = body.first_named_child().node;
+
+  if (ts_node_is_null(n)) {
+    indent.push("");
+    return;
+  }
+
+  if (ts_node_symbol(n) == sym_access_specifier) {
+    n = ts_node_next_sibling(n);
+  }
+
+  if (ts_node_symbol(n) == anon_sym_COLON) {
+    n = ts_node_next_sibling(n);
+  }
+
+  const char* begin = &current_source.top()->source[ts_node_start_byte(n)] - 1;
+  const char* end = &current_source.top()->source[ts_node_start_byte(n)];
+
+  std::string new_indent;
+
+  while (*begin != '\n' && *begin != '{') begin--;
+  if (*begin == '{') {
+    new_indent = "";
+  } else {
+    new_indent = std::string(begin + 1, end);
+  }
+
+  for (auto& c : new_indent) {
+    assert(isspace(c));
+  }
+
+  indent.push(new_indent);
+}
+
+void Cursor::pop_indent(MnNode class_body) { indent.pop(); }
+*/
+
+//------------------------------------------------------------------------------
+// Generic emit() methods
+
+CHECK_RETURN Err Cursor::start_line() {
+  Err err;
+  if (line_dirty) {
+    err << emit_char('\n');
+    err << emit_indent();
+  }
+  return err;
+}
+
+//----------------------------------------
+
+CHECK_RETURN Err Cursor::emit_backspace() {
+  Err err;
+  if (echo) {
+    LOG_C(0x8080FF, "^H");
+  }
+  str_out->pop_back();
+  return err;
+}
+
+//----------------------------------------
+
+CHECK_RETURN Err Cursor::emit_indent() {
+  Err err;
+  for (auto c : indent.top()) {
+    err << emit_char(c);
+  }
+  return err;
+}
+
+//----------------------------------------
+
+CHECK_RETURN Err Cursor::emit_char(char c, uint32_t color) {
+  Err err;
+  // We ditch all '\r'.
+  if (c == '\r') return err;
+
+  if (c < 0 || !isspace(c)) {
+    line_dirty = true;
+  }
+
+  if (c == '\n') {
+    // Strip trailing whitespace
+    while (str_out->size() && str_out->back() == ' ') {
+      err << emit_backspace();
+    }
+
+    // Discard the line if it contained only whitespace after elisions
+    if (!line_dirty && line_elided) {
+      if (echo) {
+        LOG_C(0xFF8080, " (Line elided)");
+      }
+      while (str_out->size() && str_out->back() != '\n') {
+        str_out->pop_back();
+      }
+    } else {
+      if (echo) {
+        LOG_C(0xFFFFFF, "\\n");
+      }
+      str_out->push_back(c);
+    }
+
+    line_dirty = false;
+    line_elided = false;
+  } else {
+    str_out->push_back(c);
+  }
+
+  if (echo) {
+    auto d = c;
+    if (color && d == ' ') d = '_';
+    LOG_C(color, "%c", d);
+  }
+
+  at_newline = c == '\n';
+  at_comma = c == ',';
+  return err;
+}
+
+//----------------------------------------
+
+CHECK_RETURN Err Cursor::skip_char(char c) {
+  Err err;
+  if (echo) {
+    if (c == ' ') c = '_';
+    LOG_C(0x8080FF, "%c", c);
+  }
+  return err;
+}
+
+//------------------------------------------------------------------------------
+
+Err Cursor::emit_everything() {
+  return Err();
+}
+
+//------------------------------------------------------------------------------
--- metron/parser/Cursor.hpp
+++ metron/parser/Cursor.hpp
@@ -6,9 +6,38 @@
 #include <functional>
 #include <string>
 
+#include "CSourceRepo.hpp"
+#include "CSourceFile.hpp"
+
+#include "metrolib/core/Platform.h"
+#include "metrolib/core/Err.h"
+
 struct CNode;
 
 struct Cursor {
+
+  Cursor(CSourceRepo* repo, CSourceFile* source, std::string* out);
+
+  //----------------------------------------
+
+  //void push_indent(MnNode n);
+  //void pop_indent(MnNode n);
+  CHECK_RETURN Err start_line();
+  CHECK_RETURN Err emit_backspace();
+  CHECK_RETURN Err emit_indent();
+  CHECK_RETURN Err emit_char(char c, uint32_t color = 0);
+  CHECK_RETURN Err skip_char(char c);
+
+  // Top-level emit function
+  CHECK_RETURN Err emit_everything();
+
+  //----------------------------------------
+
+  CSourceRepo* repo = nullptr;
+  CSourceFile* source = nullptr;
+  std::string* out = nullptr;
+
+  //----------------------------------------
 
   using string_to_node = std::map<std::string, CNode*>;
   using string_to_string = std::map<std::string, std::string>;
